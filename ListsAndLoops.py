407896922583df992e30ec7d1bc83fd80a4058ba
Michael Bodin <bodin.michael@gmail.com>
2022-01-03 10:45:34 -0600

    init

---
+++ ListsAndLoops.py
@@ -0,0 +1,6 @@
+acronyms = ['LOL', 'IDK', 'SMH', 'TBH']
+
+print(acronyms)
+
+for acronym in acronyms:
+    print(acronym)
---
+++ expenses.py
@@ -0,0 +1,6 @@
+expenses = [10.50, 8, 5, 15, 20, 5, 3]
+sum = 0
+for x in expenses:
+    sum = sum + x
+
+print("You spent $", sum, " on lunch this week.", sep='')
