8453de03cb158c74a6ea618700e44e64204cdbfe
Michael Bodin <bodin.michael@gmail.com>
2022-01-03 11:00:58 -0600

    request user input

--- expenses.py
+++ expenses.py
@@ -1,4 +1,8 @@
-expenses = [10.50, 8, 5, 15, 20, 5, 3]
+total = 0
+expenses = []
+for i in range(7):
+    expenses.append(float(input("Enter an expense:")))
+    
 total = sum(expenses)
 
 print("You spent $", total, " on lunch this week.", sep='')
