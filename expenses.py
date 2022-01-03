1fc4aa5fcf38434d0c73672d0c6a861dd95735fb
Michael Bodin <bodin.michael@gmail.com>
2022-01-03 10:47:50 -0600

    second

--- expenses.py
+++ expenses.py
@@ -1,6 +1,4 @@
 expenses = [10.50, 8, 5, 15, 20, 5, 3]
-sum = 0
-for x in expenses:
-    sum = sum + x
+total = sum(expenses)
 
-print("You spent $", sum, " on lunch this week.", sep='')
+print("You spent $", total, " on lunch this week.", sep='')
